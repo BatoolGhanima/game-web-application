e3d55bbc973d75852e72c2c6a07efea5a8e108fe
BatoolGhanima <batool00gh@gmail.com>
2025-03-22 13:52:05 +0300

    update router

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,8 @@
-import React from "react";
+
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from './components/home/HomePage'
 import MainContent from './components/MainContent'
 import { Layout } from "./components/Layout";
-import SideBar from "./components/SideBar";
-import Sides from "./components/sides";
 import ErrorPage from "./components/ErrorPage";
 import GameDetails from "./components/GameDetails";
 import Favorite from "./components/Favorite";
